e992a45bdce67f5a1dfc1081e62a006933760835
Derric Smith <smithde28@up.edu>
2025-02-11 19:30:15 -0800

    Initial commit

---
+++ app/src/main/java/edu/up/raindropsprogrammingassignmentderricsmith/RainView.java
@@ -0,0 +1,38 @@
+package edu.up.raindropsprogrammingassignmentderricsmith;
+
+import android.os.Build;
+import android.util.AttributeSet;
+import android.content.Context;
+import android.view.SurfaceView;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Random;
+
+public class RainView extends SurfaceView
+    {
+
+
+    protected Random rng2 = new Random();
+
+
+    public RainView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        setWillNotDraw(false);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.VANILLA_ICE_CREAM)
+    public void onDraw(Canvas canvas)
+    {
+        super.onDraw(canvas);
+        for(int i = 0; i < rng2.nextInt(6,11); i++)
+        {
+            Raindrop newDrop = new Raindrop();
+            newDrop.draw(canvas);
+        }
+    }
+}
---
+++ app/src/main/java/edu/up/raindropsprogrammingassignmentderricsmith/Raindrop.java
@@ -0,0 +1,53 @@
+package edu.up.raindropsprogrammingassignmentderricsmith;
+
+import android.graphics.Paint;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Random;
+
+public class Raindrop {
+
+    protected int x;
+
+    protected int y;
+
+    protected int size = 30;
+
+    protected Random rng = new Random();
+
+    protected Paint raindropPaint;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.VANILLA_ICE_CREAM)
+    protected void randomPaint()
+    {
+        int newColor = Color.rgb(rng.nextInt(0,256), rng.nextInt(0,256), rng.nextInt(0, 256));
+        raindropPaint = new Paint();
+        raindropPaint.setColor(newColor);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.VANILLA_ICE_CREAM)
+    public Raindrop()
+    {
+        x = rng.nextInt(0,790)+10;
+        y = rng.nextInt(0, 790)+10;
+        randomPaint();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.VANILLA_ICE_CREAM)
+    public Raindrop(int _x, int _y)
+    {
+        x = _x;
+        y = _y;
+        randomPaint();
+    }
+
+    public void draw(Canvas canvas)
+    {
+        canvas.drawOval(x, y, x+30, y+30, raindropPaint);
+    }
+}
